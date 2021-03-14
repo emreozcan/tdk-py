f861b682e96382dd3d52969cf8b745d45e52cb61
Emre Özcan <justsomechars@gmail.com>
2021-03-14 23:28:06 +0300

    Add Enum for origin languages

---
+++ tdk/classifications/__init__.py
@@ -0,0 +1,1 @@
+from .origin_languages import *
---
+++ tdk/classifications/origin_languages.py
@@ -0,0 +1,34 @@
+from enum import Enum
+
+
+class OriginLanguage(Enum):
+    ORIGINAL = 0
+    COMPOUND = 19
+
+    ARABIC = 11
+    PERSIAN = 12
+    FRENCH = 13
+    ITALIAN = 14
+    GREEK = 15
+    LATIN = 16
+    ENGLISH = 18
+    SPANISH = 20
+    ARMENIAN = 21
+    RUSSIAN = 22
+    GERMAN = 23
+    SLAVIC = 24
+    HEBREW = 25
+    HUNGARIAN = 26
+    BULGARIAN = 27
+    PORTUGUESE = 28
+    JAPANESE = 346
+    ALBANIAN = 348
+
+    MONGOLIAN = 354
+    MONGOLIAN_2 = 153
+
+    FINNISH = 392
+    ROMAIC = 393
+    SOGDIAN = 395
+    SERBIAN = 486
+    KOREAN = 420
