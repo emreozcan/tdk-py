4988548425e773a83928532eb01b820366e01d51
Emre Özcan <emre@emreis.com>
2023-02-20 11:52:32 +0300

    Remove Python 3.6 from supported version lists
    Python 3.6 doesn't have dataclasses which we rely on
    for models now.

--- setup.py
+++ setup.py
@@ -27,7 +27,6 @@ setuptools.setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
@@ -43,5 +42,5 @@ setuptools.setup(
         "": "src",
     },
     packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.6",
+    python_requires=">=3.7",
 )
