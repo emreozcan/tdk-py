c9bf9409793666c0bef3016756007dc436e1ca60
Emre Özcan <emre@emreis.com>
2024-11-09 19:05:27 +0300

    Merge models.py and gts.py

--- src/tdk/__init__.py
+++ src/tdk/__init__.py
@@ -1,2 +1,4 @@
 __version__ = "0.0.0"
 __version_tuple__ = (0, 0, 0)
+
+from tdk import alphabet, enums, gts, terms, tools
--- src/tdk/gts.py
+++ src/tdk/gts.py
@@ -1,21 +1,63 @@
-import aiohttp
-from pydantic import TypeAdapter
+from typing import List
 
-from tdk.models import Entry
+from aiohttp import ClientSession
+from pydantic import TypeAdapter, BaseModel, Field, AliasChoices
+
+from tdk.enums import ValidatedProperty, OriginLanguage
 from tdk.http import with_http_session
 from tdk.tools import lowercase, dictionary_order
 from tdk.utils import make_sync
 
 
-__all__ = [
-    "get_index",
-    "search",
-    "get_suggestion",
-]
+class Writer(BaseModel):
+    tdk_id: int = Field(validation_alias=AliasChoices("tdk_id", "yazar_id"))
+    full_name: str = Field(validation_alias=AliasChoices("full_name", "tam_adi"))
+    short_name: str = Field(validation_alias=AliasChoices("short_name", "kisa_adi"))
+
+
+class MeaningExample(BaseModel):
+    tdk_id: int = Field(validation_alias=AliasChoices("tdk_id", "ornek_id"))
+    meaning_id: int = Field(validation_alias=AliasChoices("meaning_id", "anlam_id"))
+    order: int = Field(validation_alias=AliasChoices("order", "ornek_sira"))
+    example: str = Field(validation_alias=AliasChoices("example", "ornek"))
+    writers: list[Writer] = Field(default_factory=lambda: [], validation_alias=AliasChoices("writer", "yazar"))
+
+
+class Proverb(BaseModel):
+    tdk_id: int = Field(validation_alias=AliasChoices("tdk_id", "madde_id"))
+    proverb: str = Field(validation_alias=AliasChoices("proverb", "madde"))
+    prefix: str | None = Field(validation_alias=AliasChoices("prefix", "on_taki"))
+
+
+class Meaning(BaseModel):
+    meaning: str = Field(validation_alias=AliasChoices("meaning", "anlam"))
+    tdk_id: int = Field(validation_alias=AliasChoices("tdk_id", "anlam_id"))
+    order: int = Field(validation_alias=AliasChoices("order", "anlam_sira"))
+    is_verb: bool = Field(validation_alias=AliasChoices("is_verb", "fiil"))
+    entry_id: int = Field(validation_alias=AliasChoices("entry_id", "madde_id"))
+    examples: List[MeaningExample] = Field(default_factory=list, validation_alias=AliasChoices("examples", "orneklerListe"))
+    properties: List[ValidatedProperty] = Field(default_factory=list, validation_alias=AliasChoices("properties", "ozelliklerListe"))
+
+
+class Entry(BaseModel):
+    tdk_id: int = Field(validation_alias=AliasChoices("tdk_id", "madde_id"))
+    order: int = Field(validation_alias=AliasChoices("order", "kac"))
+    entry: str = Field(validation_alias=AliasChoices("entry", "madde"))
+    plural: bool = Field(validation_alias=AliasChoices("plural", "cogul_mu"))
+    proper: bool = Field(validation_alias=AliasChoices("proper", "ozel_mi"))
+    origin_language: OriginLanguage = Field(validation_alias=AliasChoices("origin_language", "lisan_kodu"))
+    original: str = Field(validation_alias=AliasChoices("original", "lisan"))
+    entry_normalized: str | None = Field(validation_alias=AliasChoices("entry_normalized", "madde_duz"))
+    meanings: List[Meaning] = Field(default_factory=list, validation_alias=AliasChoices("meanings", "anlamlarListe"))
+    proverbs: List[Proverb] = Field(default_factory=list, validation_alias=AliasChoices("proverbs", "atasozu"))
+    pronunciation: str | None = Field(validation_alias=AliasChoices("pronunciation", "telaffuz"))
+    prefix: str | None = Field(validation_alias=AliasChoices("prefix", "on_taki"))
+    suffix: str | None = Field(validation_alias=AliasChoices("suffix", "taki"))
+
 
 
 @with_http_session
-async def get_index(*, http_session: aiohttp.ClientSession) -> list[str]:
+async def get_index(*, http_session: ClientSession) -> list[str]:
     async with (http_session.get(
         "https://sozluk.gov.tr/autocomplete.json"
     ) as response):
@@ -34,7 +76,7 @@ def get_index_sync(): ...
 
 
 @with_http_session
-async def search(query: str, /, *, http_session: aiohttp.ClientSession) \
+async def search(query: str, /, *, http_session: ClientSession) \
         -> list[Entry]:
     query = lowercase(query, keep_unknown_characters=False)
     async with http_session.get(
@@ -54,8 +96,8 @@ def search_sync(): ...
 
 
 @with_http_session
-async def get_suggestion(query: str, /, *, http_session: aiohttp.ClientSession) \
-        -> list[str]:
+async def get_suggestions(query: str, /, *,
+                          http_session: ClientSession) -> list[str]:
     async with http_session.get(
         "https://sozluk.gov.tr/oneri",
         params={"soz": query}
@@ -74,5 +116,5 @@ async def get_suggestion(query: str, /, *, http_session: aiohttp.ClientSession) 
             ) from e
 
 
-@make_sync(get_suggestion)
-def get_suggestion_sync(): ...
+@make_sync(get_suggestions)
+def get_suggestions_sync(): ...
